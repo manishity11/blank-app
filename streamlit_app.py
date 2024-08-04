aab7d2224ee8a1c9a62b78ffbf2e08d9f6524b26
manishity11 <160490232+manishity11@users.noreply.github.com>
2024-08-04 20:50:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ mobilenet_model = Model(inputs=mobilenet_model.inputs, outputs=mobilenet_model.l
 model = tf.keras.models.load_model('img_caption_model.h5')
 
 # Load the tokenizer
-with open('tokenizer.pkl', 'rb') as tokenizer_file:
+with open('features.pkl', 'rb') as tokenizer_file:
     tokenizer = pickle.load(tokenizer_file)
     
 # Set custom web page title
