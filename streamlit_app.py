925b2a494d0a278efc3f865af20a5ca5533a7181
manishity11 <160490232+manishity11@users.noreply.github.com>
2024-08-04 23:06:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,12 @@ from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_i
 from keras.applications.vgg16 import VGG16
 import keras
 from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Load MobileNetV2 model
-mobilenet_model = MobileNet(weights="imagenet")
+mobilenet_model = InceptionV3(weights="imagenet")
 mobilenet_model = Model(inputs=mobilenet_model.inputs, outputs=mobilenet_model.layers[-2].output)
 
 # Load your trained model
